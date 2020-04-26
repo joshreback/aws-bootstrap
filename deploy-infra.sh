42d86402f8100af8f7b0ecd4a7977e323fd67eaf
Josh Reback <jreback@careersofia.com>
2020-04-25 22:38:56 -0400

    codedeploy pipeline v1

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -9,12 +9,18 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 # Deploys static resources
 echo -e "\n\n============ Deploying setup.yml ============"
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
@@ -28,7 +34,14 @@ aws cloudformation deploy \
   --stack-name $STACK_NAME \
   --template-file main.yml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODE_PIPELINE_BUCKET
+
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
