e17bedbd53ba5a05e9167d762f7da600ab31f0af
Josh Reback <jreback@careersofia.com>
2020-05-09 18:21:06 -0400

    tweak start-service script

--- start-service.sh
+++ start-service.sh
@@ -7,7 +7,7 @@ REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document
 
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 
-export STACK_NAME=`aws --region $REGION ec2-describe-tags \
+export STACK_NAME=`aws --region $REGION ec2 describe-tags \
   --filters "Name=resource-id,Values=${INSTANCE_ID}" | 
             "Name=key,Values=aws:cloudformation:stack-name" \
   | jq -r ".Tags[0].Value"`
